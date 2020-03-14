c5c84d60767a55f571a12a2b4321641de4db6b6e
Second To None <secondtonone.secondtonone@gmail.com>
2020-03-14 19:05:01 +0300

    Change building

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 targets: {
                     browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
                 },
-                useBuiltIns: 'usage',
+                useBuiltIns: 'entry',
                 corejs: 3.6
             }
         ]
