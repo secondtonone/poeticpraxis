29cd8268c369b96da9892565b61c6a8056b763cb
Second To None <secondtonone.secondtonone@gmail.com>
2020-03-23 10:08:14 +0300

    Path map

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:react/recommended'],
     plugins: ['react'],
     settings: {
+        'import/resolver': 'webpack',
         react: {
             version: 'detect'
         }
