57f8792cca4b89a0e00bee63b3af07903fd2f8c5
Second To None <secondtonone.secondtonone@gmail.com>
2020-04-07 22:54:49 +0300

    2 variants Textarea

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,74 +1,37 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
+import { PureComponent } from 'preact/compat';
 import randomize from '@utils/randomize';
 
 import { FieldLabel, SimpleTextarea } from '@styles/components';
 
-export default class Textarea extends Component {
-    constructor(props) {
-        super(props);
+export default class Textarea extends PureComponent {
 
-        this.state = {
-            field: {
-                width: 0
-            }
-        };
-
-        this.delayHeightChange = 0;
-    }
-
+    field = null;
+    delayHeightChange = 0;
     componentDidMount() {
-        requestAnimationFrame(this.getBoxMeasure);
+        this.props.getRef && this.props.getRef(this.field);
+        this.heightChange();
     }
-
-    getBoxMeasure = () => {
-        // if (this.props.focus) {
-        //     this.timerID = setTimeout(() => {
-        //         this.fieldFocusing();
-        //     }, this.props.focus);
-        // }
-
-        //const width = this.field.clientWidth;
-        const height = this.field.clientHeight;
-        const offset = this.field.offsetHeight - height;
-        const lineHeight = parseInt(window.getComputedStyle(this.field, null).lineHeight, 10) || 0;
-
-        const field = {
-            //width,
-            height,
-            offset,
-            lineHeight
-        };
-
-        this.props.getMeasure(field);
-
-        this.setState({
-            field
-        });
-    };
 
     componentDidUpdate() {
-        if (this.delayHeightChange) {
-            cancelAnimationFrame(this.delayHeightChange);
-        }
-        this.delayHeightChange = requestAnimationFrame(() => this.heightChange(this.field));
+        this.delayHeightChange = requestAnimationFrame(this.heightChange);
     }
 
     componentWillUnmount() {
-        //clearTimeout(this.timerID);
         cancelAnimationFrame(this.delayHeightChange);
     }
 
-    heightChange = (element) => {
-        element.style.height = 'auto';
+    heightChange = () => {
+        this.field.style.height = 'auto';
 
-        const height = element.scrollHeight;
+        const height = this.field.scrollHeight;
 
-        element.style.height = height + this.state.field.offset + 'px';
+        this.field.style.height = height + 'px';
     };
 
-    fieldFocusing = () => {
-        this.field.focus();
-    };
+    getRef = (ref) => this.field = ref
+
+    onChange  = () => this.props.onChange && this.props.onChange()
 
     render() {
         const {
@@ -86,8 +49,7 @@ export default class Textarea extends Component {
             onMouseMove,
             onKeyDown,
             onKeyUp,
-            zoomIn,
-            onChange
+            zoomIn
         } = this.props;
 
         const id = `i${randomize()}`;
@@ -111,9 +73,9 @@ export default class Textarea extends Component {
                     zoomIn={zoomIn}
                     value={value}
                     className={className}
-                    ref={(ref) => (this.field = ref)}
+                    ref={this.getRef}
                     readOnly={readOnly}
-                    onChange={onChange ? onChange: ()=>{}}
+                    onChange={this.onChange}
                     placeholder={placeHolder}
                     aria-label="workfield"
                 />
